e396df7e301e237c5f2a2d5857517f29d73ba1e7
Arthur Buravljov <buravlev-arthur@yandex.ru>
2022-07-14 16:19:29 +0300

    Step 10: Integrated Rollbar library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import Rollbar from 'rollbar';
+import { Provider as RollbarProvider } from '@rollbar/react';
+import { Provider as  StoreProvider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -8,12 +10,20 @@ import App from './components/App';
 import store from './store/index';
 import i18n from './i18n';
 
+const rollbarConfig = {
+  accessToken: '49d539df7c9441d5a0e6c7928c743dc9',
+  environment: 'production',
+};
+
+const rollbar = new Rollbar(rollbarConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </Provider>
+  <RollbarProvider instance={rollbar}>
+    <StoreProvider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </StoreProvider>
+  </RollbarProvider>
 );
