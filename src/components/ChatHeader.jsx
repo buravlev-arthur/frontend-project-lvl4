82cc491ff6ddae2c47cd017d954909b3aafdabd8
Arthur Buravljov <buravlev-arthur@yandex.ru>
2022-07-25 17:23:48 +0300

    Check #1: ChatHeader lint errors was fixed

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 import { useTranslation } from 'react-i18next';
 import { selectors as channelsSelectors } from '../store/channelsSlice';
 import { selectors as messagesSelectors } from '../store/messagesSlice';
 
-const chatData = createSelector(
-  (state) => state,
-  (state) => {
-    const { currentChannelId } = state.channels;
-    const currentChannelName = useSelector((state) => channelsSelectors
-      .selectById(state, currentChannelId))?.name;
-    const messagesCount = useSelector(messagesSelectors.selectAll)
-      .filter(({ channelId }) => channelId === currentChannelId).length;
+const chatData = createSelector((state) => state, (allState) => {
+  const { currentChannelId } = allState.channels;
+  const currentChannelName = useSelector((state) => channelsSelectors
+    .selectById(state, currentChannelId))?.name;
+  const messagesCount = useSelector(messagesSelectors.selectAll)
+    .filter(({ channelId }) => channelId === currentChannelId).length;
 
-    return {
-      currentChannelName,
-      messagesCount,
-    };
-  }
-);
+  return { currentChannelName, messagesCount };
+});
 
 export default function ChatHeader() {
   const { currentChannelName, messagesCount } = useSelector(chatData);
